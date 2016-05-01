da3556d001042af5259e720628c2f66c087c6de8
nian0114 <268078545@qq.com>
2016-05-01 15:48:41 +0800

    FlymeOS 5 Unofficial

--- configs/prebuilt.mk
+++ configs/prebuilt.mk
@@ -1,31 +1,32 @@
 # This file is auto generate by tools/config/config_prebuilt.py
-# version: from 1602158(Nexus 5) ==> builder.20151216140920_R(Nexus 5)
-# Date: 2015/12/16 14:56
+# version: from cee4e8702d(A0001) ==> builder.20160118153457_R(Nexus 5)
+# Date: 2016/05/01 12:11
 
 BOARD_PREBUILT_DIRS += \
     app/AlarmClock \
     app/AppCenter \
     app/Calendar \
     app/Camera \
+    app/CloudDisk \
     app/ExperienceDataSync \
     app/FileManager \
     app/GameCenter \
     app/IflytekSpeechService \
-    app/LogReport \
+    app/Life \
+    app/LockScreenFramework \
+    app/Map \
     app/MzAccount \
-    app/MzBackup \
-    app/MzBlockService \
     app/MzCloudService \
+    app/MzCompaign \
     app/MzCountService \
     app/MzMPay \
+    app/MzPhoneLocationService \
     app/MzSecurity \
     app/MzSetupWizard \
+    app/MzShare \
     app/MzSimContacts \
-    app/MzSmartComService \
-    app/MzSnsSyncService \
     app/MzSyncService \
     app/MzUpdate \
-    app/NetContactService \
     app/NotePaper \
     app/O2OService \
     app/Painter \
@@ -33,237 +34,225 @@ BOARD_PREBUILT_DIRS += \
     app/Reader \
     app/Search \
     app/SystemWallpaper \
-    app/UsageStatsProvider \
-    app/WAPPushManager \
+    app/ToolBox \
     app/Weather \
+    app/vlife-sdk-mx \
     app/webview \
     customizecenter \
     framework/flyme-res \
     priv-app/AliAppAccount \
     priv-app/Browser \
-    priv-app/Contacts \
     priv-app/CustomizeCenter \
     priv-app/Dialer \
+    priv-app/EBook \
     priv-app/Email \
     priv-app/FlymeLauncher \
     priv-app/Gallery \
-    priv-app/Keyguard \
-    priv-app/Life \
-    priv-app/Mms \
+    priv-app/InCallUI \
+    priv-app/LogReport \
+    priv-app/LuckyMoney \
+    priv-app/Mms/lib \
     priv-app/Music \
+    priv-app/MusicFX \
     priv-app/MzAccountPlugin \
+    priv-app/MzBackup \
+    priv-app/MzCallSetting \
     priv-app/MzInput \
+    priv-app/NetContactService \
     priv-app/NetworkLocation_baidu \
     priv-app/PowerSaveModeHome \
+    priv-app/Scanner \
     priv-app/SoundRecorder \
     priv-app/Tag \
     priv-app/Video \
     priv-app/VoiceAssistant \
-    tts \
 
 
 BOARD_PREBUILT += \
-    app/PacProcessor/lib/arm/libjni_pacprocessor.so \
-    app/PrintSpooler/lib/arm/libprintspooler_jni.so \
     bin/flymed \
     bin/nmbd \
     bin/pppoe \
     bin/shutdownanimation \
     bin/smbd \
     bin/smbpasswd \
+    build.prop~ \
+    etc/default.mps \
     etc/permissions/com.meizu.camera.xml \
     etc/ppp/init.pppoe-pppd \
     etc/ppp/ip-up-pppoe \
-    fonts/AndroidEmoji.ttf \
-    fonts/DroidNaskh-Regular.ttf \
-    fonts/DroidNaskhUI-Regular.ttf \
-    fonts/DroidSansArmenian.ttf \
-    fonts/DroidSansGeorgian.ttf \
-    fonts/DroidSansHebrew-Bold.ttf \
-    fonts/DroidSansHebrew-Regular.ttf \
-    fonts/DroidSerif-Bold.ttf \
-    fonts/DroidSerif-BoldItalic.ttf \
-    fonts/DroidSerif-Italic.ttf \
-    fonts/DroidSerif-Regular.ttf \
+    fonts/DINPro-Black.otf \
+    fonts/DINPro-Bold.otf \
+    fonts/DINPro-Light.otf \
+    fonts/DINPro-Medium.otf \
+    fonts/DINPro-Regular.otf \
+    fonts/DroidSansFallback-flyme.ttf \
+    fonts/Flyme-Light.ttf \
     fonts/SourceHanSansCN-ExtraLight.ttf \
+    fonts/SourceHanSansCN-Light.otf \
     fonts/SourceHanSansCN-Normal.ttf \
     fonts/SourceHanSansJP-Normal.ttf \
     fonts/SourceHanSansKR-Normal.ttf \
+    fonts/ZawgyiOne.ttf \
     framework/com.meizu.camera.jar \
     framework/flyme-framework.jar \
     framework/flyme-telephony-common.jar \
     framework/meizu2_jcifs.jar \
-    framework/telephony-meizu.jar \
-    lib/libBMapApiEngine_v1_3_5.so \
-    lib/libDClient.so \
     lib/libDominantColors.so \
+    lib/libEQGraphLib.so \
     lib/libFace.so \
-    lib/libHAOMA.so \
     lib/libIvw35.so \
-    lib/libMzGameCenterLib-jni.so \
-    lib/libRSSupport.so \
     lib/libRefocusModule.so \
-    lib/libTmsdk-2.0.7-mfr.so \
-    lib/libams-1.1.4-64b-mfr.so \
+    lib/libTmsdk-2.0.8-mfr.so \
+    lib/libamapv303.so \
+    lib/libamapv303ex.so \
+    lib/libams-1.1.5-m-mfr.so \
+    lib/libams-1.1.6-64b-mfr.so \
     lib/libaudioinfo_jni.so \
-    lib/libbdpush_V1_0.so \
-    lib/libblur.so \
-    lib/libbspatch.so \
-    lib/libdce-1.1.2-mfr.so \
+    lib/libdce-1.1.5-mfr.so \
     lib/libdlna_jni.so \
     lib/libeffects_filters.so \
     lib/libeffects_mosaic.so \
-    lib/libem_aitalk5.so \
-    lib/libentryexpro.so \
     lib/libexif_gallery.so \
-    lib/libfile_jni.so \
     lib/libimage_codec.so \
     lib/libimage_dehazing.so \
     lib/libimageproc.so \
     lib/libipc_pref.527.1.so \
     lib/libjni_gallery_utils.so \
     lib/libjni_gauss_blur.so \
+    lib/libjni_glrenderer.so \
     lib/libkanbox_meizu_gallery_sdk.so \
-    lib/liblocSDK4d.so \
-    lib/liblocSDK_2_5OEM.so \
+    lib/liblocSDK5c.so \
     lib/libmcode_image.so \
-    lib/libmeizucamera.so \
     lib/libmobilesafe360-sdk-jni-524.2.so \
     lib/libnative_blur.so \
+    lib/libnative_glrenderer.so \
     lib/libnetworklocation.so \
     lib/libpatch-1.0.1.so \
     lib/libphoto_process.so \
-    lib/libpj_opensl_dev.so \
-    lib/libpjsipjni.so \
     lib/librender_engine.so \
     lib/libskia_hw_interface.so \
     lib/libskia_private.so \
-    lib/libstlport_shared.so \
+    lib/libspecific.so \
     lib/libtaglib.so \
-    lib/libttsaisound.so \
+    lib/libtbt.so \
     lib/libttscompat.so \
     lib/libttspico.so \
-    lib/libttsplusmsc.so \
+    lib/libttssuitemsc.so \
     lib/libupdate-jni-1.0.1.2002.so \
-    media/audio/alarms/After_school.ogg \
-    media/audio/alarms/Beep.ogg \
-    media/audio/alarms/Blues.ogg \
-    media/audio/alarms/Bon_voyage.ogg \
-    media/audio/alarms/Buzzer.ogg \
-    media/audio/alarms/Call_of_love.ogg \
-    media/audio/alarms/Crow.ogg \
-    media/audio/alarms/Crystal.ogg \
-    media/audio/alarms/Digital.ogg \
-    media/audio/alarms/Dreamland.ogg \
-    media/audio/alarms/East_ville_memory.ogg \
-    media/audio/alarms/Encounter.ogg \
-    media/audio/alarms/Fantasy_city.ogg \
-    media/audio/alarms/Guitar.ogg \
-    media/audio/alarms/Heartbeat.ogg \
-    media/audio/alarms/Home.ogg \
-    media/audio/alarms/Life.ogg \
-    media/audio/alarms/Machinery_ringtone.ogg \
-    media/audio/alarms/Marimba.ogg \
-    media/audio/alarms/Mario.ogg \
-    media/audio/alarms/Mischief.ogg \
-    media/audio/alarms/Morning.ogg \
-    media/audio/alarms/Morning_Dew.ogg \
-    media/audio/alarms/Morning_run.ogg \
-    media/audio/alarms/Music_box.ogg \
-    media/audio/alarms/Nighty-night.ogg \
-    media/audio/alarms/Passion.ogg \
-    media/audio/alarms/Phone_ring.ogg \
-    media/audio/alarms/Piano.ogg \
-    media/audio/alarms/Progress.ogg \
-    media/audio/alarms/Rain_dance.ogg \
-    media/audio/alarms/Running_in_the_wind.ogg \
-    media/audio/alarms/Simplicity.ogg \
-    media/audio/alarms/Soaring.ogg \
-    media/audio/alarms/Summer_joys.ogg \
-    media/audio/alarms/Technology.ogg \
-    media/audio/alarms/That_afternoon.ogg \
-    media/audio/alarms/The_World_Cup.ogg \
-    media/audio/alarms/The_cantor.ogg \
-    media/audio/alarms/The_kittens.ogg \
-    media/audio/alarms/The_pitch.ogg \
-    media/audio/alarms/Tumbler.ogg \
-    media/audio/alarms/Twilight_dance.ogg \
-    media/audio/alarms/Wake_up.ogg \
-    media/audio/alarms/Waltz.ogg \
-    media/audio/notifications/Agile.ogg \
-    media/audio/notifications/Arrival.ogg \
-    media/audio/notifications/Beating.ogg \
-    media/audio/notifications/Blow.ogg \
-    media/audio/notifications/Chocolate.ogg \
-    media/audio/notifications/Cleverer.ogg \
-    media/audio/notifications/Code_Word.ogg \
-    media/audio/notifications/Corner.ogg \
-    media/audio/notifications/Doorbell.ogg \
-    media/audio/notifications/Drop.ogg \
-    media/audio/notifications/Empire.ogg \
-    media/audio/notifications/Gift.ogg \
-    media/audio/notifications/Jump.ogg \
-    media/audio/notifications/Laputa.ogg \
-    media/audio/notifications/Little_Crystal.ogg \
-    media/audio/notifications/Meteor.ogg \
-    media/audio/notifications/Naughty.ogg \
-    media/audio/notifications/Promotion.ogg \
-    media/audio/notifications/Pure.ogg \
-    media/audio/notifications/Red_Wine_Cork.ogg \
-    media/audio/notifications/Ripples.ogg \
-    media/audio/notifications/Shining.ogg \
-    media/audio/notifications/Silent_Night.ogg \
-    media/audio/notifications/Spining.ogg \
-    media/audio/notifications/Tapping.ogg \
-    media/audio/notifications/The_North_Star.ogg \
-    media/audio/notifications/Triumph.ogg \
-    media/audio/notifications/Tunnel.ogg \
-    media/audio/notifications/VideoRecord.ogg \
-    media/audio/ringtones/After_school.ogg \
-    media/audio/ringtones/Beep.ogg \
-    media/audio/ringtones/Blues.ogg \
-    media/audio/ringtones/Bon_voyage.ogg \
-    media/audio/ringtones/Buzzer.ogg \
-    media/audio/ringtones/Call_of_love.ogg \
-    media/audio/ringtones/Crow.ogg \
-    media/audio/ringtones/Crystal.ogg \
-    media/audio/ringtones/Digital.ogg \
-    media/audio/ringtones/Dreamland.ogg \
-    media/audio/ringtones/East_ville_memory.ogg \
-    media/audio/ringtones/Encounter.ogg \
-    media/audio/ringtones/Fantasy_city.ogg \
-    media/audio/ringtones/Guitar.ogg \
-    media/audio/ringtones/Heartbeat.ogg \
-    media/audio/ringtones/Home.ogg \
-    media/audio/ringtones/Life.ogg \
-    media/audio/ringtones/Machinery_ringtone.ogg \
-    media/audio/ringtones/Marimba.ogg \
-    media/audio/ringtones/Mario.ogg \
-    media/audio/ringtones/Mischief.ogg \
-    media/audio/ringtones/Morning.ogg \
-    media/audio/ringtones/Morning_dew.ogg \
-    media/audio/ringtones/Morning_run.ogg \
-    media/audio/ringtones/Music_box.ogg \
-    media/audio/ringtones/Nighty-night.ogg \
-    media/audio/ringtones/Passion.ogg \
-    media/audio/ringtones/Phone_ring.ogg \
-    media/audio/ringtones/Piano.ogg \
-    media/audio/ringtones/Progress.ogg \
-    media/audio/ringtones/Rain_dance.ogg \
-    media/audio/ringtones/Running_in_the_wind.ogg \
-    media/audio/ringtones/Simplicity.ogg \
-    media/audio/ringtones/Soaring.ogg \
-    media/audio/ringtones/Summer_joys.ogg \
-    media/audio/ringtones/Technology.ogg \
-    media/audio/ringtones/That_afternoon.ogg \
-    media/audio/ringtones/The_World_Cup.ogg \
-    media/audio/ringtones/The_cantor.ogg \
-    media/audio/ringtones/The_kittens.ogg \
-    media/audio/ringtones/The_pitch.ogg \
-    media/audio/ringtones/Tumbler.ogg \
-    media/audio/ringtones/Twilight_dance.ogg \
-    media/audio/ringtones/Wake_up.ogg \
-    media/audio/ringtones/Waltz.ogg \
+    lib/libvlife_media.so \
+    lib/libvlife_openglutil.so \
+    lib/libvlife_render.so \
+    lib/libwebviewchromium.so \
+    media/audio/alarms/01_Piano.ogg \
+    media/audio/alarms/02_Digital.ogg \
+    media/audio/alarms/03_Flyme.ogg \
+    media/audio/alarms/04_Music_box.ogg \
+    media/audio/alarms/05_Technology.ogg \
+    media/audio/alarms/06_Guitar.ogg \
+    media/audio/alarms/07_Marimba.ogg \
+    media/audio/alarms/08_Mischief.ogg \
+    media/audio/alarms/09_Crystal.ogg \
+    media/audio/alarms/10_Progress.ogg \
+    media/audio/alarms/11_The_kittens.ogg \
+    media/audio/alarms/12_Morning_song.ogg \
+    media/audio/alarms/13_Call_of_love.ogg \
+    media/audio/alarms/14_Tumbler.ogg \
+    media/audio/alarms/15_Journey_begins.ogg \
+    media/audio/alarms/16_New_life.ogg \
+    media/audio/alarms/17_Heartbeat.ogg \
+    media/audio/alarms/18_East_Ville_memory.ogg \
+    media/audio/alarms/19_Waltz.ogg \
+    media/audio/alarms/20_The_cantor.ogg \
+    media/audio/alarms/21_Passion.ogg \
+    media/audio/alarms/22_Journey.ogg \
+    media/audio/alarms/23_Game.ogg \
+    media/audio/alarms/24_Rain_dance.ogg \
+    media/audio/alarms/25_Twilight_dance.ogg \
+    media/audio/alarms/26_Foredawn.ogg \
+    media/audio/alarms/27_Home.ogg \
+    media/audio/alarms/28_Blues.ogg \
+    media/audio/alarms/29_Dash.ogg \
+    media/audio/alarms/30_Machine.ogg \
+    media/audio/alarms/31_Telephone.ogg \
+    media/audio/alarms/32_Beeper.ogg \
+    media/audio/alarms/33_After_school.ogg \
+    media/audio/alarms/34_Mario.ogg \
+    media/audio/alarms/35_Nighty_night.ogg \
+    media/audio/alarms/36_Soaring.ogg \
+    media/audio/alarms/37_Childhood.ogg \
+    media/audio/alarms/38_Morning_run.ogg \
+    media/audio/alarms/39_Sweetheart.ogg \
+    media/audio/alarms/40_Curtain_call.ogg \
+    media/audio/notifications/01_Triumph.ogg \
+    media/audio/notifications/02_Reminder.ogg \
+    media/audio/notifications/03_Doorbell.ogg \
+    media/audio/notifications/04_Promotion.ogg \
+    media/audio/notifications/05_Little_Crystal.ogg \
+    media/audio/notifications/06_Arrival.ogg \
+    media/audio/notifications/07_Cleverer.ogg \
+    media/audio/notifications/08_Agile.ogg \
+    media/audio/notifications/09_Red_Wine_Cork.ogg \
+    media/audio/notifications/10_Tapping.ogg \
+    media/audio/notifications/11_Drop.ogg \
+    media/audio/notifications/12_Connect.ogg \
+    media/audio/notifications/13_Spining.ogg \
+    media/audio/notifications/14_Gift.ogg \
+    media/audio/notifications/15_Blow.ogg \
+    media/audio/notifications/16_Pure.ogg \
+    media/audio/notifications/17_Beating.ogg \
+    media/audio/notifications/18_Naughty.ogg \
+    media/audio/notifications/19_Chocolate.ogg \
+    media/audio/notifications/20_Code_Word.ogg \
+    media/audio/notifications/21_Corner.ogg \
+    media/audio/notifications/22_Empire.ogg \
+    media/audio/notifications/23_Jump.ogg \
+    media/audio/notifications/24_Laputa.ogg \
+    media/audio/notifications/25_Meteor.ogg \
+    media/audio/notifications/26_Ripples.ogg \
+    media/audio/notifications/27_Shining.ogg \
+    media/audio/notifications/28_Silent_Night.ogg \
+    media/audio/notifications/29_The_North_Star.ogg \
+    media/audio/notifications/30_The_time_tunne.ogg \
+    media/audio/ringtones/01_Piano.ogg \
+    media/audio/ringtones/02_Digital.ogg \
+    media/audio/ringtones/03_Flyme.ogg \
+    media/audio/ringtones/04_Music_box.ogg \
+    media/audio/ringtones/05_Technology.ogg \
+    media/audio/ringtones/06_Guitar.ogg \
+    media/audio/ringtones/07_Marimba.ogg \
+    media/audio/ringtones/08_Mischief.ogg \
+    media/audio/ringtones/09_Crystal.ogg \
+    media/audio/ringtones/10_Progress.ogg \
+    media/audio/ringtones/11_The_kittens.ogg \
+    media/audio/ringtones/12_Morning_song.ogg \
+    media/audio/ringtones/13_Call_of_love.ogg \
+    media/audio/ringtones/14_Tumbler.ogg \
+    media/audio/ringtones/15_Journey_begins.ogg \
+    media/audio/ringtones/16_New_life.ogg \
+    media/audio/ringtones/17_Heartbeat.ogg \
+    media/audio/ringtones/18_East_Ville_memory.ogg \
+    media/audio/ringtones/19_Waltz.ogg \
+    media/audio/ringtones/20_The_cantor.ogg \
+    media/audio/ringtones/21_Passion.ogg \
+    media/audio/ringtones/22_Journey.ogg \
+    media/audio/ringtones/23_Game.ogg \
+    media/audio/ringtones/24_Rain_dance.ogg \
+    media/audio/ringtones/25_Twilight_dance.ogg \
+    media/audio/ringtones/26_Foredawn.ogg \
+    media/audio/ringtones/27_Home.ogg \
+    media/audio/ringtones/28_Blues.ogg \
+    media/audio/ringtones/29_Dash.ogg \
+    media/audio/ringtones/30_Machine.ogg \
+    media/audio/ringtones/31_Telephone.ogg \
+    media/audio/ringtones/32_Beeper.ogg \
+    media/audio/ringtones/33_After_school.ogg \
+    media/audio/ringtones/34_Mario.ogg \
+    media/audio/ringtones/35_Nighty_night.ogg \
+    media/audio/ringtones/36_Soaring.ogg \
+    media/audio/ringtones/37_Childhood.ogg \
+    media/audio/ringtones/38_Morning_run.ogg \
+    media/audio/ringtones/39_Sweetheart.ogg \
+    media/audio/ringtones/40_Curtain_call.ogg \
     media/audio/ui/boot.ogg \
     media/audio/ui/camera_click_burst.ogg \
     media/audio/ui/charged.ogg \
@@ -274,17 +263,34 @@ BOARD_PREBUILT += \
     media/audio/ui/recorder_prompt_cn.ogg \
     media/audio/ui/recorder_prompt_en.ogg \
     media/audio/ui/recorder_prompt_end.ogg \
+    media/launcher.xml \
     media/shutanimation.zip \
-    priv-app/DefaultContainerService/lib/arm/libdefcontainer_jni.so \
 
 
 BOARD_PRESIGNED_APPS += \
     AliAppAccount.apk \
+    Bluetooth.apk \
+    EBook.apk \
+    GmsCore.apk \
+    GoogleCalendarSyncAdapter.apk \
+    GoogleContactsSyncAdapter.apk \
+    GoogleFeedback.apk \
+    GoogleLoginService.apk \
+    GoogleServicesFramework.apk \
     IflytekSpeechService.apk \
     Life.apk \
+    LockScreenFramework.apk \
+    Map.apk \
     MzAccountPlugin.apk \
     NetworkLocation_baidu.apk \
+    O2OService.apk \
+    Phonesky.apk \
+    Velvet.apk \
+    WAPPushManager.apk \
+    WebViewGoogle.apk \
     qcrilmsgtunnel.apk \
+    vlife-sdk-mx.apk \
+    webview.apk \
 
 
  # This is the end.
